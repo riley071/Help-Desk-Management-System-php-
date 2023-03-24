c453b3cd4cf359f48f7a5417b406a927c9332403
Riley matewere <81653537+riley071@users.noreply.github.com>
2023-03-24 22:00:43 +0100

    Add files via upload

---
+++ admin/send.php
@@ -0,0 +1,41 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'phpmailer/src/Exception.php';
+require 'phpmailer/src/PHPMailer.php';
+require 'phpmailer/src/SMTP.php';
+
+if(isset($_POST["send"])){
+    $mail = new PHPMailer(true);
+
+    $mail->isSMTP();
+    $mail->Host = 'smtp.gmail.com';
+    $mail->SMTPAuth = true;
+    $mail->Username = 'emzomatewere@gmail.com';   
+    $mail->Password = 'hddvnksjhmevuamn';
+    $mail->SMTPSecure = 'ssl';
+    $mail->Port = 465;
+
+    $mail->setFrom('emzomatewere@gmail.com');
+
+    $mail->addAddress($_POST["email"]);
+
+    $mail->isHTML(true);
+    $mail->Subject ='Youre compliant has been fixed';
+    $mail->Body = $_POST["message"];
+    $mail->send();
+
+    echo
+    "
+    <script>
+    alert('Sent Succesfully');
+    document.location=href = 'closed-complaint.php';
+    </script>
+    ";
+}
+
+
+
+
+?>
